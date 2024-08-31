6f18a64213e629c32196d0e3867d2a7c0729da3b
anlongawf <anan123456a123@gmail.com>
2024-08-31 20:19:39 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+// First page
+window.onload = function() {
+    showPage('explore');
+};
+
+
+
+
+function showPage(page) {
+    document.getElementById('explore').style.display = 'none';
+    document.getElementById('libary').style.display = 'none';
+    document.getElementById('podcast').style.display = 'none';
+    document.getElementById('live').style.display = 'none';
+    document.getElementById('top-music').style.display = 'none';
+    document.getElementById('playlist').style.display = 'none';
+    document.getElementById('upload').style.display = 'none';
+    
+
+    document.getElementById(page).style.display = 'block';
+}
+
+// Change img
+
+function moveCards() {
+    const galleryItem = document.querySelector('.gallery-item');
+    const cards = Array.from(galleryItem.querySelectorAll('.card'));
+
+    if (cards.length > 0) {
+        const firstCard = cards[0];
+        galleryItem.appendChild(firstCard);
+    }
+}
+
+
+setInterval(moveCards, 3000);
